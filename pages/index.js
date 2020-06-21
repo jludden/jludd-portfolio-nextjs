33db0ee69e967138aa89399361aa27cbb01d43f3
Jason Ludden <jasonludden@gmail.com>
2020-06-21 21:45:21 +0000

    nextjs with tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,36 @@
 import Head from 'next/head'
+import Layout from '../components/Layout'
 
-export default function Home() {
+const Index = ({ title, description, ...props }) => {
+  return (
+    <Layout pageTitle={title}>
+      <h1 className="title">Welcome</h1>
+      <p className="dsecription">
+        {description}
+      </p>
+      <main>
+        content here
+      </main>
+    </Layout>
+  )
+}
+
+export default Index
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  }
+}
+
+
+// export default function Home() {
+function Home() {
   return (
     <div className="container">
       <Head>
